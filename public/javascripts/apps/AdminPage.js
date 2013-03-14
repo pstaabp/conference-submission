5517f6362153a5e4d077fdfc9bfd2beb9021bcea
Peter Staab <peter.staab@gmail.com>
2013-03-14 11:59:56 -0400

    Added the oral presentation scheduling.

--- public/javascripts/apps/AdminPage.js
+++ public/javascripts/apps/AdminPage.js
@@ -17,18 +17,21 @@ require.config({
         'Backbone': { deps: ['underscore', 'jquery'], exports: 'Backbone'},
         'bootstrap':['jquery'],
         'backbone-validation': ['Backbone'],
-        'jquery-ui': ['jquery']
+        'jquery-ui': ['jquery'],
+        'XDate':{ exports: 'XDate'}
     }
 });
 
 require(['Backbone', 'underscore',
     '../models/UserList','../models/User','../models/ProposalList',
-    '../models/Proposal','../views/EditableCell',
+    '../models/Proposal','../views/EditableCell', '../views/WebPage',
     './common','bootstrap','jquery-ui'],
-function(Backbone, _, UserList,User,ProposalList,Proposal,EditableCell,common){
+function(Backbone, _, UserList,User,ProposalList,Proposal,EditableCell,WebPage,common){
 
-    var AdminPage = Backbone.View.extend({
+    var AdminPage = WebPage.extend({
         initialize: function () {
+            this.constructor.__super__.initialize.apply(this, {el: this.el});
+
             _.bindAll(this, 'render','usersFetched','proposalsFetched');  // include all functions that need the this object
             var self = this;
             
@@ -38,14 +41,22 @@ function(Backbone, _, UserList,User,ProposalList,Proposal,EditableCell,common){
             this.users.fetch({success: this.usersFetched});
             this.proposals.on("add",this.render);
 
+            this.proposals.on("change",function(theChange){
+                var p = (_(theChange.changed).pairs())[0];
+                self.announce.addMessage("The " + p[0] + " changed to " + p[1]);
+            });
 
-            //this.render();
+
+            this.proposalParams = [{class: ".session", field: "session"},{class:".title",field: "title"},
+                {class: ".sponsor-name", field: "sponsor_name"}, {class: ".sponsor-email", field: "sponsor_email"},
+                {class: ".sponsor-dept", field: "sponsor_dept"}, {class: ".type", field: "type"}];
 
             $("#logout").on("click",common.logout);   
 
         },
-        render: function ()
-        {
+        render: function () {
+            this.constructor.__super__.render.apply(this);  // Call  WebPage.render(); 
+
             var self = this;
 
             var userTable = this.$("#user-table tbody");
@@ -53,11 +64,10 @@ function(Backbone, _, UserList,User,ProposalList,Proposal,EditableCell,common){
                 userTable.append(_.template($("#user-row-template").html(),_user.attributes));
             });
 
-            var proposalTable = this.$("#proposal-table");
-            this.proposals.each(function(prop){
-                proposalTable.append(_.template($("#proposal-row-template").html(),prop.attributes));
+            var proposalTable = $("#proposal-table");
+            this.proposals.each(function(proposal){
+                proposalTable.append((new ProposalView({model: proposal, proposalParams: self.proposalParams})).render().el);
             });
-
             
             this.oralPresentTable.render();
             
@@ -117,42 +127,78 @@ function(Backbone, _, UserList,User,ProposalList,Proposal,EditableCell,common){
             this.oralPresentTable = new OralPresentationScheduleView({el: $("#schedule"), proposals: oralPresentations});
 
             this.render();
+        },
+        // This function parses the proposal table and makes each item editable
+        setEditable: function () {
+
         }
+    });
+
+    var ProposalView = Backbone.View.extend({
+        initialize: function(){
+            _.bindAll(this,"render");
+
+        },
+        render: function() {
+            var self = this;
+            this.$el.html(_.template($("#proposal-row-template").html(),this.model.attributes));
+            _(this.options.proposalParams).each(function(prop){
+                self.$(prop.class).html((new EditableCell({model: self.model, property: prop.field})).render().el);    
+            })
+            return this;
+        },
+
     });
 
     var OralPresentationScheduleView = Backbone.View.extend({
         rowTemplate: _.template($("#proposal-row-template").html()),
         initialize: function(){
-            this.proposals = this.options.proposals; 
+            _.bindAll(this,"render","reorder");
+            this.proposals = this.options.proposals;
+            console.log(this.proposals); 
 
         },
         render: function (){
+
+            var sessionNames = "ABCDEFGHIJKL"
             
-            var numCols = 3
+            var numCols = 6
               , i =0; 
             var tableBody = this.$("#oral-present-table tbody tr");
             var tableHead = this.$("#oral-present-table thead tr");
             for(i=0;i< numCols; i++){
                 tableBody.append("<td><ul class='oral-present-col' id='col" + i + "'></ul></td>");
-                tableHead.append("<th>Column " + i + "</td>");
+                tableHead.append("<th>Session " + sessionNames.charAt(i) + "</td>");
             }
 
+            var sortedProposals = _(this.proposals).sort(function(prop){ return prop.get("session")});
+            var re = /OP-(\d)-(\d)/;
 
-            _(this.proposals).each(function(prop,i){
-                this.$("#col0").append(_.template($("#oral-presentation-template").html(),prop.attributes));
-                this.$("#col1").append(_.template($("#oral-presentation-template").html(),prop.attributes));
+            _(this.proposals).each(function(prop){
+                var matches = prop.get("session").match(re);
+                console.log(matches);
+                this.$("#col" + matches[1]).append(_.template($("#oral-presentation-template").html(),_.extend(prop.attributes, {cid: prop.cid})));
                 self.$("#oral-present-table .oral-present:last").attr("data-id",prop.cid);
             });
 
-            $("#col0, #col1, #col2").sortable({update: function(evt,ui){
-                console.log("reordering!");
-                self.$("#oral-present-table .oral-present").each(function(i,prop){
-                    console.log(prop);
-                
-                }
-                )}
-                , connectWith: ".oral-present-col"}).disableSelection();
+            //$("#col0, #col1, #col2").sortable({items: "li", connectWith: ".oral-present-col"});
 
+            $(".oral-present-col").sortable({ 
+                connectWith: ".oral-present-col", 
+                                placeholder: "ui-state-highlight",
+                                receive: this.reorder});
+
+
+        },
+        reorder: function (){
+            var self = this; 
+            this.$("li").each(function(i,item){
+                var cid = $(item).attr("id")
+                var proposal = _(self.proposals).find(function(prop) { return prop.cid===cid;})
+                var list = parseInt($(item).parent().attr("id").split("col")[1]); 
+                proposal.set("session","OP-"+list + "-"+$(item).index());
+                proposal.save();
+            })
         }
     });
 
