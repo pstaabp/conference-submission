8599d6f5b3179fc598d76a81356de0a908a64978
Peter Staab <peter.staab@gmail.com>
2013-04-01 11:23:02 -0400

    Updated Emails in admin page to include emails from other authors.

--- public/javascripts/apps/AdminPage.js
+++ public/javascripts/apps/AdminPage.js
@@ -391,9 +391,16 @@ function(Backbone, _, UserList,User,ProposalList,Proposal,Judge,JudgeList,Editab
             this.parent = this.options.parent;
         },
         render: function() {
-            var _allParticipants = this.parent.users.pluck("email").join(", ");
-            var _oralPresenters = _.chain(this.parent.getOrals()).pluck("attributes").pluck("email").unique().value().join(", ");
+            var _allUsers = this.parent.users.pluck("email");
+            var _otherAuthors = _.chain(this.parent.getProposals()).pluck("attributes").pluck("other_authors")
+                                    .flatten().pluck("email").value(); 
+            var _allParticipants = _.unique(_allUsers,_otherAuthors).join(", ");
+            var _oralPresenters = _.chain(this.parent.getOrals()).pluck("attributes").pluck("email").unique().value();
+            var _oralPresentersOther = _.chain(this.parent.getOrals()).pluck("attributes").pluck("other_authors")
+                                            .flatten().pluck("email").union(_oralPresenters).value().join(", ");
             var _posterPresenters = _.chain(this.parent.getPosters()).pluck("attributes").pluck("email").unique().value().join(", ");
+            var _posterPresentersOther = _.chain(this.parent.getOrals()).pluck("attributes").pluck("other_authors")
+                                            .flatten().pluck("email").union(_posterPresenters).value();
             var _missingNames = _(this.parent.users.filter(function(user) { return user.get("first_name")==="";}))
                                     .chain().pluck("attributes").pluck("email").unique().value().join(", ");
             var _sponsors = _.chain(this.parent.getProposals()).pluck("attributes").pluck("sponsor_email").unique().value().join(", ");
@@ -405,7 +412,7 @@ function(Backbone, _, UserList,User,ProposalList,Proposal,Judge,JudgeList,Editab
 
 
             this.$el.html(_.template($("#emails-template").html(),{allParticipants: _allParticipants,
-                    oralPresenters: _oralPresenters, posterPresenters: _posterPresenters,
+                    oralPresenters: _oralPresentersOther, posterPresenters: _posterPresentersOther,
                     missingNames: _missingNames, sponsors: _sponsors, missing_statements: _missing_statments,
                     judges: _judges}));
 
