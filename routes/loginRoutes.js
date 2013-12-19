e688ee0c7220871640419fc40d297bcb10ecfb5e
Peter Staab <peter.staab@gmail.com>
2013-12-19 12:52:25 -0500

    changes to the login

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,5 +1,6 @@
 var ldap = require('ldapjs');
 var ldap_settings = require('../ldap');
+var _ = require("underscore");
 
 module.exports = function loginRoutes(app,User,routeUser,LoginToken,loadUser) {
 
@@ -13,19 +14,19 @@ module.exports = function loginRoutes(app,User,routeUser,LoginToken,loadUser) {
 
 	app.post('/conference-submission/login',function(req,res){
 		console.log(req.body);
-		console.log(ldap_settings);
+		//console.log(ldap_settings);
 		//console.log({falconkey: req.body.user.falconkey, password: req.body.user.password});
 
 		//res.render('login.jade',{user: {falconkey: req.body.user.falconkey}, msg: "Your username and password are not correct. Please try again." })
 
 		// lookup the User in the DB
 
-		client.bind(req.body.user.falconkey,req.body.user.password, function(err,_res){
+		client.bind("fscad\\"+req.body.user.falconkey,req.body.user.password, function(err,_res){
 			if(_res){ 
 
 				User.findOne({falconkey: req.body.user.falconkey},function(err2,_user){
 
-					if(_user){
+				if(_user){
 						req.session.user_id = _user.id;
 
 						// save a cookie
@@ -36,10 +37,16 @@ module.exports = function loginRoutes(app,User,routeUser,LoginToken,loadUser) {
 		        		}); 
 				
 					} else { //the user isn't in the database yet
-						res.json({"msg": "The user isn't in the database"});
+						client.search("",{filter: "(&(sAMAccountName="+req.body.user.falconkey+"))",scope: "sub"},function(_err2,_res2){
+							_res2.on('searchEntry', function(entry) {
+								res.json({first_name: entry.object.givenName, last_name: entry.object.sn,email:entry.object.mail , other: entry.object.description});
+});
+						});
 					}
-				}
-			});
+				});
+			} else { // the password was incorrect
+				res.render('login.jade',{user: {falconkey: req.body.user.falconkey}, msg: "Your username and password are not correct. Please try again." })
+			}
 		});
 	});
 
