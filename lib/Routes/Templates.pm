e0b9eea5d827c9f25f0d887fae16150a10184101
Peter Staab <peter.staab@gmail.com>
2017-03-20 12:45:52 -0400

    Another fix to a bad link

--- lib/Routes/Templates.pm
+++ lib/Routes/Templates.pm
@@ -42,9 +42,6 @@ get '/login' => sub {
 
 post '/login' => sub {
     debug "in post /login";
-
-    debug body_parameters; 
-
     my ($success, $realm) = authenticate_user(body_parameters->{username},body_parameters->{password});
 
     if ($success) {
